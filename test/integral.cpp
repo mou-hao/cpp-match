e21f9647eb5a8f66a1a6fbe7b66dc8ba66db075b
Hao Mou <hao.mou@outlook.com>
2022-07-14 16:30:41 +0800

    Use CMake and add test

---
+++ test/integral.cpp
@@ -0,0 +1,62 @@
+#include "match.hpp"
+#include <gtest/gtest.h>
+
+using namespace mat;
+
+TEST(IntegralTest, ExprMatch) {
+  EXPECT_THROW({
+    match(1) (
+      pattern(2) = []() {},
+      pattern(1) = []() { throw 1; });
+  }, int);
+}
+
+TEST(IntegralTest, WildcardMatch) {
+  EXPECT_THROW({
+    match(1) (
+      pattern(_) = []() { throw 1; },
+      pattern(1) = []() {});
+  }, int);
+}
+
+TEST(IntegralTest, CaptureMatch) {
+  EXPECT_THROW({
+    match(1) (
+      pattern(cpt) = [](auto x) {
+        EXPECT_EQ(x, 1);
+        throw 1;
+      },
+      pattern(1) = []() {});
+  }, int);
+}
+
+TEST(IntegralTest, NonVoidMatch) {
+  int x = match(1) (
+    pattern(1) = []() { return 100; },
+    pattern(2) = []() { return 200; });
+  EXPECT_EQ(x, 100);
+}
+
+TEST(IntegralTest, VoidNoMatch) {
+  EXPECT_NO_THROW({
+    match(1) (
+      pattern(2) = []() {},
+      pattern(3) = []() {});
+  });
+}
+
+TEST(IntegralTest, NonVoidNoMatch) {
+  EXPECT_THROW({
+    match(1) (
+      pattern(2) = []() { return 2; },
+      pattern(3) = []() { return 2; });
+  }, exceptions::NonVoidNoMatch);
+}
+
+TEST(IntegralTest, AltPattern) {
+  EXPECT_THROW({
+    match(1) (
+      pattern(0) | pattern(1) = []() { throw 1; },
+      pattern(2) = []() {});
+  }, int);
+}
